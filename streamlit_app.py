15375dc1b2f455c32d461dbbc8da0b72c74cb0ab
hemanth-hamy <hemanthfusionqa@gmail.com>
2025-07-27 22:57:09 +0530

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,6 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="Fusion AI Immortal", layout="centered")
-st.title("⚛️ Fusion AI Immortal Core")
-st.success("🚀 Your Streamlit app is running successfully on Render!")
